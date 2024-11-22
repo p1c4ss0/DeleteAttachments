03cf9a19b80416486c0396192a4df34dc344659c
p1c4ss0 <94533364+p1c4ss0@users.noreply.github.com>
2024-11-21 19:58:14 -0600

    Create DeleteAttachments.js

---
+++ DeleteAttachments.js
@@ -0,0 +1,17 @@
+Office.onReady(function() {
+  // ... (button click event handler)
+
+  function deleteAttachments() {
+    Office.context.mailbox.item.load('attachments');
+    Office.context.mailbox.item.retrieveAsync().then(function() {
+      var attachments = Office.context.mailbox.item.attachments;
+      attachments.forEach(function(attachment) {
+        attachment.deleteAsync().then(function() {
+          console.log('Attachment deleted: ' + attachment.name);
+        }, function(error) {
+          console.error('Error deleting attachment: ' + error);
+        });
+      });
+    });
+  }
+});
