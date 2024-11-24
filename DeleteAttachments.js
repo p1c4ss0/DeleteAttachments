2a3e05234ccb1e82927109699cc25a3a069aa117
p1c4ss0 <94533364+p1c4ss0@users.noreply.github.com>
2024-11-24 14:52:00 -0600

    Update DeleteAttachments.js

--- DeleteAttachments.js
+++ DeleteAttachments.js
@@ -1,25 +1,11 @@
 Office.onReady(function (info) {
     if (info.host === Office.HostType.Outlook) {
-        document.getElementById("removeButton").onclick = deleteAttachments;
+        document.getElementById("removeButton").onclick = countAttachments;
     }
 });
 
-function deleteAttachments() {
+function countAttachments() {
     var item = Office.context.mailbox.item;
     var totalAttachments = item.attachments.length;
-    
-    if (totalAttachments > 0) {
-        console.log('Total attachments:', totalAttachments);
-        item.attachments.forEach(function (attachment) {
-            item.removeAttachmentAsync(attachment.id, function (result) {
-                if (result.status === Office.AsyncResultStatus.Failed) {
-                    console.error('Failed to remove attachment:', result.error.message);
-                } else {
-                    console.log('Attachment removed:', attachment.name);
-                }
-            });
-        });
-    } else {
-        console.log("No attachments to remove.");
-    }
+    console.log('Total attachments:', totalAttachments);
 }
